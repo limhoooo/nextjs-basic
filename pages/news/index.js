a9572fcf42df37d4272324fcc63db67cd324a23a
lim ho <dlagh123@gmail.com>
2023-04-06 10:03:26 +0900

    fetch TEST

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 
 const NewsPage = () => {
+  useEffect(async () => {
+    console.log("aaaa");
+    const aaa = await fetch("https://api.github.com//users/limhoooo/repos");
+    console.log(aaa);
+  }, []);
   return (
     <>
-      <h1>news page</h1>
+      <h1>news paasdvasdvsadge</h1>
       <ul>
         <li>
           <Link href="/news/nextjs-is-a-1">link 1</Link>
