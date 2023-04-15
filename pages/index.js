bbf2de45522540fb673acbadc3bd6c374f115e7c
lim ho <dlagh123@gmail.com>
2023-04-15 16:21:15 +0900

    next js 프로젝트 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import MeetupList from "./../components/meetups/MeetupList";
 
-const Homepage = () => {
-  return <h1>home page</h1>;
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meet up",
+    image: "https://picsum.photos/600/600/?random",
+    address: "Some address 5, 12345 Some City",
+    description: "This a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A 2 Meet up",
+    image: "https://picsum.photos/600/600/?random",
+    address: "222Some address 5, 12345 Some City",
+    description: "222This a first meetup",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
-export default Homepage;
+export default HomePage;
